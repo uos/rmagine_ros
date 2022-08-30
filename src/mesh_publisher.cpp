244aef2489b892c9c2bed68ee61c076090dc5011
Alexander Mock <amock@uos.de>
2022-08-30 18:20:37 +0200

    temporary fix

--- src/mesh_publisher.cpp
+++ src/mesh_publisher.cpp
@@ -8,15 +8,17 @@
 
 using namespace rmagine;
 
-mesh_msgs::MeshGeometry embreeToRos(const EmbreeMesh& mesh, Matrix4x4 T)
+mesh_msgs::MeshGeometry embreeToRos(EmbreeMeshPtr mesh, Matrix4x4 T)
 {
     mesh_msgs::MeshGeometry mesh_ros;
 
     // Vertices
-    for(int i=0; i<mesh.Nvertices; i++)
+    auto vertices = mesh->verticesTransformed();
+    for(int i=0; i<vertices.size(); i++)
     {
         geometry_msgs::Point vertex_ros;
-        auto vt = T * mesh.vertices[i];
+        // mesh->ver
+        auto vt = T * vertices[i];
         vertex_ros.x = vt.x;
         vertex_ros.y = vt.y;
         vertex_ros.z = vt.z;
@@ -24,17 +26,48 @@ mesh_msgs::MeshGeometry embreeToRos(const EmbreeMesh& mesh, Matrix4x4 T)
     }
 
     // Faces
-    for(int i=0; i<mesh.Nfaces; i++)
+    auto faces = mesh->faces();
+    for(int i=0; i<faces.size(); i++)
     {
         mesh_msgs::TriangleIndices face_ros;
-        face_ros.vertex_indices[0] = mesh.faces[i].v0;
-        face_ros.vertex_indices[1] = mesh.faces[i].v1;
-        face_ros.vertex_indices[2] = mesh.faces[i].v2;
+        face_ros.vertex_indices[0] = faces[i].v0;
+        face_ros.vertex_indices[1] = faces[i].v1;
+        face_ros.vertex_indices[2] = faces[i].v2;
         mesh_ros.faces.push_back(face_ros);
     }
 
     return mesh_ros;
 }
+
+std::vector<mesh_msgs::MeshGeometry> embreeToRos(
+    EmbreeScenePtr scene, 
+    Matrix4x4 T = Matrix4x4::Identity())
+{
+    std::vector<mesh_msgs::MeshGeometry> ret;
+
+    for(auto elem : scene->geometries())
+    {
+        size_t geom_id = elem.first;
+        EmbreeInstancePtr inst = std::dynamic_pointer_cast<EmbreeInstance>(elem.second);
+        if(inst)
+        {
+            Matrix4x4 T_ = T * inst->matrix();
+
+            std::vector<mesh_msgs::MeshGeometry> ret_ = embreeToRos(inst->scene(), T_);
+            ret.insert(ret.end(), ret_.begin(), ret_.end());
+        } else {
+            EmbreeMeshPtr mesh = std::dynamic_pointer_cast<EmbreeMesh>(elem.second);
+            if(mesh)
+            {
+                // leaf
+                ret.push_back(embreeToRos(mesh, T));
+            }
+        }
+    }
+
+    return ret;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -50,60 +83,38 @@ int main(int argc, char** argv)
 
     auto map = importEmbreeMap(meshfile);
 
-    std::vector<mesh_msgs::MeshGeometryStamped> meshes_ros;
+    // std::vector<mesh_msgs::MeshGeometryStamped> meshes_ros;
 
-    for(size_t i=0; i<map->instances.size(); i++)
+    std::vector<mesh_msgs::MeshGeometry> meshes_ros = embreeToRos(map->scene);
+
+    std::vector<mesh_msgs::MeshGeometryStamped> meshes_stamped(meshes_ros.size());
+
+    for(size_t i=0; i<meshes_stamped.size(); i++)
     {
-        auto instance = map->instances[i];
-
-        auto mesh = instance->mesh();
-
-        std::cout << "Creating mesh from instance: " << instance->instID << ", geom: " << mesh->geomID << std::endl; 
-        mesh_msgs::MeshGeometryStamped mesh_ros;
-        mesh_ros.header.frame_id = map_frame;
-        mesh_ros.header.stamp = ros::Time::now();
         std::stringstream ss;
-        ss << i;
-        mesh_ros.uuid = ss.str();
-        std::cout << "Converting Mesh " << ss.str() << std::endl;
-        mesh_ros.mesh_geometry = embreeToRos(*mesh, instance->T);
-
-        meshes_ros.push_back(mesh_ros);
+        ss << "mesh/" << i;
+        meshes_stamped[i].mesh_geometry = meshes_ros[i];
+        meshes_stamped[i].header.frame_id = map_frame;
+        meshes_stamped[i].uuid = ss.str();
+        std::cout << meshes_stamped[i].uuid << std::endl;
     }
 
-
-    // for(size_t i=0; i<map->meshes.size(); i++)
-    // {
-    //     mesh_msgs::MeshGeometryStamped mesh_ros;
-    //     mesh_ros.header.frame_id = map_frame;
-    //     mesh_ros.header.stamp = ros::Time::now();
-    //     std::stringstream ss;
-    //     ss << i;
-    //     mesh_ros.uuid = ss.str();
-    //     std::cout << "Converting Mesh " << ss.str() << std::endl;
-    //     mesh_ros.mesh_geometry = embreeToRos(*map->meshes[i]);
-    //     meshes_ros.push_back(mesh_ros);
-    // }
+    // ros::Publisher mesh_pub = nh_p.advertise<mesh_msgs::MeshGeometryStamped>("map_mesh", 100);
 
     std::vector<ros::Publisher> mesh_pubs;
-
-    for(auto mesh : meshes_ros)
+    for(size_t i=0; i<meshes_stamped.size(); i++)
     {
-        std::stringstream ss;
-        ss << "mesh/" << mesh.uuid;
-        mesh_pubs.push_back(nh_p.advertise<mesh_msgs::MeshGeometryStamped>(ss.str(), 1));
+        mesh_pubs.push_back(nh_p.advertise<mesh_msgs::MeshGeometryStamped>(meshes_stamped[i].uuid, 10));
     }
-
-    // ros::Publisher mesh_pub = nh_p.advertise<mesh_msgs::MeshGeometryStamped>("mesh", 1);
 
     ros::Rate r(0.1);
 
-    std::cout << "Publishing Mesh ..." << std::endl;
+    std::cout << "Publishing Meshes (" << meshes_stamped.size() << ") ..." << std::endl;
     while(ros::ok())
     {
-        for(size_t i=0; i<meshes_ros.size(); i++)
+        for(size_t i=0; i<meshes_stamped.size(); i++)
         {
-            auto mesh = meshes_ros[i];
+            auto mesh = meshes_stamped[i];
             auto pub = mesh_pubs[i];
             mesh.header.stamp = ros::Time::now();
             pub.publish(mesh);
